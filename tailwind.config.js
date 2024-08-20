5ee5d6f1b38fe7414f17ef6a085d1aa96ffa734b
Kostas Boukouvalas <mpoukouvalas.kk@gmail.com>
2024-08-20 17:09:34 +0300

    setup tailwindcss postcss - autoprefixer - routing

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      spacing: {
+        '67': '67px',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors:{
+        semiGray:'#828282'
+      },
+      lineHeight: {
+        '30': '30px', // Custom class 'leading-custom-30'
+        '44': '44px'
+      },
+    },
+  },
+  plugins: [],
+}
+
