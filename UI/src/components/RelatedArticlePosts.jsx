a353e25e8245cef46b2f63dc2591e4995859f25b
Kostas Boukouvalas <mpoukouvalas.kk@gmail.com>
2024-08-30 16:36:29 +0300

    fix some issues with images on relatedPosts

--- UI/src/components/RelatedArticlePosts.jsx
+++ UI/src/components/RelatedArticlePosts.jsx
@@ -15,18 +15,18 @@ export default function RelatedArticlePosts({ posts }) {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-0 md:gap-8">
       {posts.map((post, index) => (
         <div key={index} className="pb-10 md:pb-20">
-          <div className="h-64 md:h-80 w-full overflow-hidden rounded-lg">
+          <div className="h-64 md:h-96 w-full overflow-hidden rounded-lg">
             <a href={`/post?id=${post?.id}`}>
               <img
                 src={post.url}
                 alt={`image_${post.title}`}
-                className="rounded-lg"
+                className="w-full h-auto object-cover max-h-[427px] rounded-lg "
               />
             </a>
           </div>
 
           {/* Title and Author */}
-          <div className="w-full py-4 md:py-6">
+          <div className="w-full py-4 md:py-0">
             <a href={`/post?id=${post?.id}`}>
               <h1 className="font-inter text-lg md:text-xl leading-6 md:leading-30 text-black font-medium">
                 {post.title}
