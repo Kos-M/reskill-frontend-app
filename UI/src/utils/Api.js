889fe1dc5f085fc5244f0ee1b077722f04fb0eab
Kostas Boukouvalas <mpoukouvalas.kk@gmail.com>
2024-09-04 15:29:00 +0300

    change a bit backend structure and update Api host config to run locally

--- UI/src/utils/Api.js
+++ UI/src/utils/Api.js
@@ -2,8 +2,7 @@ import axios from "axios";
 let Api;
 
 Api = axios.create({
-  // baseURL: "http://127.0.0.1:3000/",
-  baseURL: "http://192.168.1.51:3000/",
+  baseURL: "http://127.0.0.1:3000/",
 });
 
 Api.defaults.headers.common["Cache-Control"] = "no-cache";
